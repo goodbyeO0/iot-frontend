55b86ce378e3561c3ee08bccba47cbd812213dfc
goodbyeO0 <zikrizhan@gmail.com>
2024-07-05 04:27:18 +0800

    v3

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 import { FaTemperatureHigh } from "react-icons/fa";
 import { TbAirConditioning } from "react-icons/tb";
 import { GiEntryDoor } from "react-icons/gi";
+import { IoHome } from "react-icons/io5";
 
 function Navbar() {
     return (
         <>
             <div className={`flex justify-center min-w-full  container bg-[#f8f8f8]`}>
                 <div className={`font-semibold w-3/4 min-h-16  flex justify-evenly items-center`}>
+                    <Link to={"/"}><IoHome size="25px" /></Link>
                     <Link to={"/temperature"}><FaTemperatureHigh size="25px" /></Link>
                     <Link to={"/fan"}><TbAirConditioning size="25px" /></Link>
                     <Link to={"/entry"}><GiEntryDoor size="25px" /></Link>
